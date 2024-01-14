f6544d3d61c36c3988b60689cd056e7c4258e25f
Lolitron <Ovcharov_05@mail.ru>
2024-01-14 23:58:38 +0300

    b

--- server.js
+++ server.js
@@ -45,3 +45,5 @@ app.post("/comments", async function (request, response) {
 const listener = app.listen(process.env.PORT, function () {
 	console.log("Your app is listening on port " + listener.address().port);
 });
+
+module.exports = app;
