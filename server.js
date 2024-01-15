18e7ba930032baf94f6a1aa7edb329df3a0e40a0
Lolitron <Ovcharov_05@mail.ru>
2024-01-15 19:01:30 +0300

    axios

--- server.js
+++ server.js
@@ -1,32 +1,62 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+import axios from "axios";
 
 const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json()); // for parsing application/json
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get("/", function (req, res) {
 	// const databases = await notion.search({
 	// 	filter: {
 	// 	  property: "object",
 	// 	  value: "database",
 	// 	},
 	//   })
-	response.sendFile("index.html");
+	res.sendFile("index.html");
 });
 
-app.get("/auth", (request, response) => {
-	response.json({
-		body: request.body,
-		query: request.query,
-		params: request.params,
-		path: request.path,
-	})
+app.get("/auth", (req, res) => {
+	const options = {
+		method: "POST",
+		url: "https://api.notion.com/v1/oauth/token",
+		headers: {
+			accept: "application/json",
+			"content-type": "application/json",
+			Authorization:
+				"Basic " +
+				Buffer.from(
+					process.env.NOTION_CLIENT_ID +
+						":" +
+						process.env.NOTION_CLIENT_SECRET,
+					"base64"
+				),
+		},
+		data: {
+			grant_type: "authorization_code",
+			code: req.query.code,
+			redirect_uri: "https://notion-auth.vercel.app",
+		},
+	};
+	axios
+		.request(options)
+		.then(function (response) {
+			res.json(response.data);
+		})
+		.catch(function (error) {
+			console.error(error);
+		});
+	res.json({
+		body: req.body,
+		query: req.query,
+		params: req.params,
+		path: req.path,
+	});
 });
 
 // Create new page comments. The page ID is provided in the web form.
