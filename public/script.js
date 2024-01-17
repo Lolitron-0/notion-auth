5b56163d0c50bd14416867e8e0e51ce5b7562994
Lolitron <Ovcharov_05@mail.ru>
2024-01-18 00:42:01 +0300

    rework

--- public/script.js
+++ public/script.js
@@ -1,16 +1,36 @@
 window.onload = () => {
 	const urlParams = new URLSearchParams(window.location.search);
+
 	if (urlParams.get("success") === "1") {
 		document.getElementById("content").innerHTML = `
     <p class="title">Успешная авторизация!</p>
     <p class="text">Можно возвращаться в бота</p>
     `;
 		document.getElementById("success-icon").style.display = "block";
-	} else if(urlParams.get("error") === "1"){
-        document.getElementById("content").innerHTML = `
+	} else if (urlParams.get("error") === "1") {
+		document.getElementById("content").innerHTML = `
         <p class="title">Произошла ошибка!</p>
         <p class="text">Свяжитесь с <a class="link" href="https://t.me/lolitron">@lolitron</a> в Telegram</p>
         `;
 		document.getElementById("error-icon").style.display = "block";
+	} else if (urlParams.get("family_id")) {
+		localStorage.setItem("family_id", urlParams.get("family_id"));
+	} else {
+		var req = new XMLHttpRequest();
+		req.open("POST", "auth");
+		req.setRequestHeader("Content-Type", "application/json");
+		req.send(
+			JSON.stringify({
+				code: urlParams.get("code"),
+				family_id: localStorage.getItem("family_id"),
+			})
+		);
+		req.onreadystatechange = function () {
+			if (req.readyState == 4 && req.status == 200) {
+				window.location.href = "/?success=1";
+			} else {
+				window.location.href = "/?error=1";
+			}
+		};
 	}
 };
--- server.js
+++ server.js
@@ -2,26 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const axios = require("axios");
-
-const { Client } = require("@notionhq/client");
-const notion = new Client({ auth: process.env.NOTION_KEY });
+const cookieParser = require("cookie-parser");
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static(__dirname + "/public"));
 app.use(express.json()); // for parsing application/json
+app.use(cookieParser());
+
+const families_clients = new Map();
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-	// const databases = await notion.search({
-	// 	filter: {
-	// 	  property: "object",
-	// 	  value: "database",
-	// 	},
-	//   })
 	res.sendFile("index.html");
 });
 
-app.get("/auth", async (req, res) => {
+app.post("/auth", async (req, res) => {
 	const options = {
 		method: "POST",
 		url: "https://api.notion.com/v1/oauth/token",
@@ -36,17 +31,59 @@ app.get("/auth", async (req, res) => {
 		},
 		data: {
 			grant_type: "authorization_code",
-			code: req.query.code,
-			redirect_uri: "http://localhost/auth",
+			code: req.body.code,
+			redirect_uri: "http://localhost",
 		},
 	};
 	axios
 		.request(options)
-		.then(function (response) {
+		.then(async function (response) {
+			const { Client } = require("@notionhq/client");
+			const notion = new Client({ auth: response.data.access_token });
+
+			const databases = await notion.search({
+				filter: {
+					property: "object",
+					value: "database",
+				},
+			});
+			let dbId;
+			for (const res of databases.results) {
+				if (res.title[0].plain_text == "Хранилище") dbId = res.id;
+			}
+			const events = await notion.databases.query({
+				database_id: dbId,
+				filter: {
+					and: [
+						{
+							property: "Тип",
+							select: {
+								equals: "Событие",
+							},
+						},
+					],
+				},
+			});
+			const result = { family_id: req.body.family_id, arr: [] };
+			for (const e of events.results) {
+				result.arr.push({
+					name: e.properties["Имя"].title[0].plain_text,
+					data: e.properties["Даты"].date,
+				});
+			}
+			console.log(result);
+			const options = {
+				method: "POST",
+				url: "https://api.notion.com/v1/oauth/token",
+				data: {
+					grant_type: "authorization_code",
+					code: req.query.code,
+					redirect_uri: "http://localhost/auth",
+				},
+			};
 			res.redirect("/?success=1");
 		})
 		.catch(function (error) {
-			console.log(error.response);
 			res.redirect("/?error=1");
 		});
 });
