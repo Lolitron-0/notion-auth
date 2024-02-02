02ae6b5ac620d5901b69d93647269329828a160e
Lolitron <Ovcharov_05@mail.ru>
2024-02-02 20:54:50 +0300

    query stuff + notion page edit

--- public/map-script.js
+++ public/map-script.js
@@ -1,63 +1,50 @@
-ymaps.ready(function () {
-	let myMap = new ymaps.Map(
-		"map",
-		{
-			center: [55.76, 37.64],
-			zoom: 10,
-		},
-		{
-			searchControlProvider: "yandex#search",
+window.onload = () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	if (urlParams.get("update") === "1") {
+		document.getElementById("map").style.display = "none";
+		document.getElementById("upd-request").style.display = "flex";
+	} else {
+		const places = [];
+		const query = urlParams.get("coords").split(",");
+		for (let i = 0; i < query.length - 2; i++) {
+			places.push({
+				name: query[0],
+				coords: [parseFloat(query[i + 1]), parseFloat(query[i + 2])],
+			});
 		}
-	);
-	myMap.geoObjects.add(
-		new ymaps.Placemark(
-			[55.687086, 37.529789],
-			{
-				balloonContent: "Place",
-			},
-			{
-				preset: "islands#circleIcon",
-				iconColor: "#8d5bc1",
+
+		ymaps.ready(function () {
+			let myMap = new ymaps.Map(
+				"map",
+				{
+					center: places[0] ? places[0].coords : [55.76, 37.64],
+					zoom: 10,
+				},
+				{
+					searchControlProvider: "yandex#search",
+				}
+			);
+
+			for (const place of places) {
+				myMap.geoObjects.add(
+					new ymaps.Placemark(
+						place.coords,
+						{
+							balloonContent: place.name,
+						},
+						{
+							preset: "islands#circleIcon",
+							iconColor: "#8d5bc1",
+						}
+					)
+				);
 			}
-		)
-	)
-    myMap.geoObjects.add(
-		new ymaps.Placemark(
-			[55.677016, 37.549789],
-			{
-				balloonContent: "Place",
-			},
-			{
-				preset: "islands#circleIcon",
-				iconColor: "#8d5bc1",
-			}
-		)
-	)
-    myMap.geoObjects.add(
-		new ymaps.Placemark(
-			[55.681086, 37.519719],
-			{
-				balloonContent: "Place",
-			},
-			{
-				preset: "islands#circleIcon",
-				iconColor: "#8d5bc1",
-			}
-		)
-	)
-    myMap.geoObjects.add(
-		new ymaps.Placemark(
-			[55.617086, 37.521689],
-			{
-				balloonContent: "Place",
-			},
-			{
-				preset: "islands#circleIcon",
-				iconColor: "#8d5bc1",
-			}
-		)
-	)
-    document.querySelectorAll("[class*='ground-pane']").forEach((el)=>{
-        el.style.filter = "saturate(0.1)";
-    })
-});
+
+			document
+				.querySelectorAll("[class*='ground-pane']")
+				.forEach((el) => {
+					el.style.filter = "saturate(0.1)";
+				});
+		});
+	}
+};
--- server.js
+++ server.js
@@ -14,16 +14,14 @@ async function requestEvents(access_token, family_id) {
 	const notion = new Client({ auth: access_token });
 
 	const databases = await notion.search({
+		query: "Хранилище",
 		filter: {
 			property: "object",
 			value: "database",
 		},
 	});
-	let dbId;
-	for (const res of databases.results) {
-		//console.log(res.title[0].plain_text);
-		if (res.title[0].plain_text == "Хранилище") dbId = res.id;
-	}
+	let dbId = databases.results[0].id;
+
 	const events = await notion.databases.query({
 		database_id: dbId,
 		filter: {
@@ -61,7 +59,96 @@ app.get("/map", function (req, res) {
 	console.log(req.headers);
 	console.log(req.data);
 	console.log(req.query);
-	res.sendFile(staticDir+"/map.html");
+	res.sendFile(staticDir + "/map.html");
+});
+
+/*
+	{
+		"access_token": "token"
+	}
+*/
+app.post("/update_places", async function (req, res) {
+	const { Client } = require("@notionhq/client");
+	const notion = new Client({ auth: req.body.access_token });
+
+	const pageQuery = await notion.search({
+		query: "Карта",
+		filter: {
+			property: "object",
+			value: "page",
+		},
+	});
+	const pageId = pageQuery.results[0].id;
+
+	const pageContent = await notion.blocks.children.list({
+		block_id: pageId,
+		page_size: 50,
+	});
+
+	let embedBlock;
+	for (const block of pageContent.results) {
+		if (block.type === "embed") embedBlock = block;
+	}
+
+	const databases = await notion.search({
+		query: "Хранилище",
+		filter: {
+			property: "object",
+			value: "database",
+		},
+	});
+	let dbId = databases.results[0].id;
+
+	const places = await notion.databases.query({
+		database_id: dbId,
+		filter: {
+			and: [
+				{
+					property: "Тип",
+					select: {
+						equals: "Место",
+					},
+				},
+			],
+		},
+	});
+
+	const coords = [];
+	let x, y;
+	for (const place of places.results) {
+		x = place.properties["Широта"].rich_text[0];
+		y = place.properties["Долгота"].rich_text[0];
+		if (x && y) {
+			x = parseFloat(x.plain_text);
+			y = parseFloat(y.plain_text);
+			if (x != NaN && y != NaN) {
+				coords.push([place.properties["Имя"].title.plain_text, x, y]);
+			}
+		}
+	}
+
+	// https://notion-auth.vercel.app/map?update=1
+	let url = "https://notion-auth.vercel.app/map?coords=";
+	for (const coord of coords) {
+		url +=
+			coord[0] +
+			"," +
+			coord[1].toString() +
+			"," +
+			coord[2].toString() +
+			",";
+	}
+	if (url.at(-1) === ",") url = url.slice(0, url.length - 1);
+
+	const response = await notion.blocks.update({
+		block_id: embedBlock.id,
+		embed: {
+			caption: [],
+			url: url,
+		},
+	});
+
+	res.json(places);
 });
 
 app.post("/auth", async (req, res) => {
@@ -88,6 +175,7 @@ app.post("/auth", async (req, res) => {
 	axios
 		.request(options)
 		.then(async function (bearerAuthResponse) {
+			console.log(bearerAuthResponse.data.access_token);
 			const eventsResult = await requestEvents(
 				bearerAuthResponse.data.access_token,
 				req.body.family_id
