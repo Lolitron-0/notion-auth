1697a7f6b05620d2d99204b6e269b905adfe0802
Lolitron <Ovcharov_05@mail.ru>
2024-02-02 15:37:46 +0300

    map colors

--- public/map-script.js
+++ public/map-script.js
@@ -17,8 +17,47 @@ ymaps.ready(function () {
 			},
 			{
 				preset: "islands#circleIcon",
-				iconColor: "#3caa3c",
+				iconColor: "#8d5bc1",
 			}
 		)
-	);
+	)
+    myMap.geoObjects.add(
+		new ymaps.Placemark(
+			[55.677016, 37.549789],
+			{
+				balloonContent: "Place",
+			},
+			{
+				preset: "islands#circleIcon",
+				iconColor: "#8d5bc1",
+			}
+		)
+	)
+    myMap.geoObjects.add(
+		new ymaps.Placemark(
+			[55.681086, 37.519719],
+			{
+				balloonContent: "Place",
+			},
+			{
+				preset: "islands#circleIcon",
+				iconColor: "#8d5bc1",
+			}
+		)
+	)
+    myMap.geoObjects.add(
+		new ymaps.Placemark(
+			[55.617086, 37.521689],
+			{
+				balloonContent: "Place",
+			},
+			{
+				preset: "islands#circleIcon",
+				iconColor: "#8d5bc1",
+			}
+		)
+	)
+    document.querySelectorAll("[class*='ground-pane']").forEach((el)=>{
+        el.style.filter = "saturate(0.1)";
+    })
 });
