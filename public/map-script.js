8001af6ea755464cd99c0d68cd10acef5a327ca7
Lolitron-0 <ovcharov_05@mail.ru>
2024-05-02 23:22:39 +0300

    map loading

--- public/map-script.js
+++ public/map-script.js
@@ -1,73 +1,86 @@
+function update() {
+	const loadingContainer = document.getElementById("loading")
+	loadingContainer.style.visibility = "visible"
+	loadingContainer.style.opacity = 1
+
+	const urlParams = new URLSearchParams(window.location.search);
+	const id = urlParams.get("id");
+
+	var req = new XMLHttpRequest();
+	req.open("POST", "map_data");
+	req.setRequestHeader("Content-Type", "application/json");
+
+	req.send(
+		JSON.stringify({
+			id: id,
+		})
+	);
+
+
+	req.onreadystatechange = function () {
+		if (req.readyState != 4 || req.status != 200) {
+			return;
+		}
+
+		const places = JSON.parse(req.responseText);
+
+		ymaps.ready(function () {
+			let myMap = new ymaps.Map(
+				"map",
+				{
+					center: places[0] ? places[0].coords : [55.76, 37.64],
+					zoom: 10,
+				},
+				{
+					searchControlProvider: "yandex#search",
+				}
+			);
+
+			for (const place of places) {
+				myMap.geoObjects.add(
+					new ymaps.GeoObject(
+						{
+							geometry: {
+								type: "Point",
+								coordinates: place.coords,
+							},
+							properties: {
+								//iconContent: place.name,
+								hintContent: place.name,
+								balloonContent: place.name,
+							},
+						},
+						{
+							preset: "islands#circleIcon",
+							iconColor: "#8d5bc1",
+						}
+					)
+				);
+			}
+
+			document
+				.querySelectorAll("[class*='ground-pane']")
+				.forEach((el) => {
+					el.style.filter = "saturate(0.1)";
+				});
+
+			loadingContainer.style.visibility = "hidden"
+			loadingContainer.style.opacity = 0
+		});
+
+	}
+}
+
 window.onload = () => {
 	const urlParams = new URLSearchParams(window.location.search);
 	if (urlParams.get("update") === "1") {
 		document.getElementById("map").style.display = "none";
 		document.getElementById("upd-request").style.display = "flex";
 	} else {
-		const urlParams = new URLSearchParams(window.location.search);
-		const id = urlParams.get("id");
-
-		var req = new XMLHttpRequest();
-		req.open("POST", "map_data");
-		req.setRequestHeader("Content-Type", "application/json");
-
-		req.send(
-			JSON.stringify({
-				id: id,
-			})
-		);
-
-
-		req.onreadystatechange = function () {
-			if (req.readyState != 4 || req.status != 200) {
-				return;
-			}
-
-			const places = JSON.parse(req.responseText);
-			console.log(places);
-
-
-			ymaps.ready(function () {
-				let myMap = new ymaps.Map(
-					"map",
-					{
-						center: places[0] ? places[0].coords : [55.76, 37.64],
-						zoom: 10,
-					},
-					{
-						searchControlProvider: "yandex#search",
-					}
-				);
-
-				for (const place of places) {
-					myMap.geoObjects.add(
-						new ymaps.GeoObject(
-							{
-								geometry: {
-									type: "Point",
-									coordinates: place.coords,
-								},
-								properties: {
-									//iconContent: place.name,
-									hintContent: place.name,
-									balloonContent: place.name,
-								},
-							},
-							{
-								preset: "islands#circleIcon",
-								iconColor: "#8d5bc1",
-							}
-						)
-					);
-				}
-
-				document
-					.querySelectorAll("[class*='ground-pane']")
-					.forEach((el) => {
-						el.style.filter = "saturate(0.1)";
-					});
-			});
-
-		}
+		update();
 	}
+
+	document.getElementById("update-btn").onclick = update;
 };
+
+
